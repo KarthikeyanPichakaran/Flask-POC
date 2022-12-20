4084f16dbf3ef05e7b3f089bb06cbdde87086d1d
KarthikeyanPichakaran <108862105+KarthikeyanPichakaran@users.noreply.github.com>
2022-12-20 19:09:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_marshmallow import Marshmallow
-
+import json, os
+import boto3
+from botocore.exceptions import NoCredentialsError
+from configparser import ConfigParser
+import logging
 
 #app initialization
 app = Flask(__name__)
@@ -11,28 +15,29 @@ app.secret_key = "Secret_Key" #session
 
 #basic authentication
 auth = HTTPBasicAuth()
-users = {"flask": generate_password_hash("bankapp")}
-
-@auth.verify_password
-def verify_password(username, password):
-    if username in users and check_password_hash(users.get(username), password):
-        return username
-
-#data serializaion and deserialization
-ma = Marshmallow(app)
-
-class BankSchema(ma.Schema):
-    class Meta:
-        fields = ("customer_id", "name", "bank_name", "address", "contact", "account_type")
-
-#custdata table schema
-bank_schema = BankSchema()
-bank_schema = BankSchema(many=True)
 
 #db connection
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/bankapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+#write error log
+logging.basicConfig(filename="log.txt",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.info)
+
+#file upload from loacl to s3
+def upload_to_aws(resp_json_file, bucket, s3path):
+    try:
+        s3resource.upload_file(resp_json_file, bucket, s3path)
+        return "True"
+    except FileNotFoundError:
+        logging.error(f"Json file not found {resp_json_file}")
+    except NoCredentialsError:
+        logging.error(f"Access issue to upload the file")
 
 #DB model creation
 class Custdata(db.Model):
@@ -53,22 +58,42 @@ class Custdata(db.Model):
 with app.app_context():
     db.create_all()
 
+#Data serializaion
+class BankSchema(ma.Schema):
+    class Meta:
+        fields = ("customer_id", "name", "bank_name", "address", "contact", "account_type")
+
+#custdata table schema
+bank_schema = BankSchema()
+bank_schema = BankSchema(many=True)
+
+'''@app.route('/')
+def display():
+    return render_template("users.html")'''
+
+'''@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['name']
+        customer_id = request.form['id']
+        user = Custdata.query.filter_by(customer_id = customer_id).first()
+        if user == None:
+            flash('User Exist')
+            return "Welcome to Flask Application"
+        else:
+            flash('User does not exist')
+            return "You are not an authourized person"'''
+
+#Fetch all customer details
 @app.route('/custdata', methods=['GET'])
-@auth.login_required
 def get_customer_details():
     all_data = Custdata.query.all()
-    return bank_schema.jsonify(all_data) #API Json result
-    #return render_template("Index.html", customers= all_data) #HTML result 
+    return bank_schema.jsonify(all_data)
 
+#Add new customer details    
 @app.route('/custdata', methods=['POST'])
 def add_customer():
     if request.method == 'POST':
-        #name = request.form['name']
-        #bank_name = request.form['bname']
-        #address = request.form['address']
-        #contact = request.form['contact']
-        #account_type = request.form['acctype']
-        
         name = request.json['name']
         bank_name = request.json['bank_name']
         address = request.json['address']
@@ -78,19 +103,16 @@ def add_customer():
         new_data = Custdata(name, bank_name, address, contact, account_type)
         db.session.add(new_data)
         db.session.commit()
-
-        #flash("New Customer Added Successfully")
         return "Data added successfully"
-        #return redirect(url_for('Index'))
 
-#get particular customer data
+#get specific customer data
 @app.route("/custdata/<customer_id>", methods=["GET"])
 def get_detail(customer_id):
     if request.method == 'GET':
         my_data = Custdata.query.filter_by(customer_id = customer_id)
-        #print(my_data)
         return bank_schema.jsonify(my_data)
         
+#Update the existing customer details
 @app.route("/custdata/<customer_id>", methods=['PUT'])
 def update(customer_id):
     if request.method == 'PUT':
@@ -102,18 +124,50 @@ def update(customer_id):
         my_data.account_type = request.json['account_type']
 
         db.session.commit()
-        #flash("Customer Data Updated Successfully")
-        #return redirect(url_for('Index'))
         return "Data modified successfully"
 
+#Delete the specific customer
 @app.route('/custdata/<customer_id>', methods=['DELETE'])
 def delete(customer_id):
     my_data = Custdata.query.get(customer_id)
     db.session.delete(my_data)
     db.session.commit()
-    return "Data deleted successfully"
-    #flash("Customer Data Deleted Successfully")
-    #return redirect(url_for('Index'))
+    return f"Record {customer_id} deleted successfully"
 
+#Read the config file
+config_data = ConfigParser()
+config_data.read("config.ini")
+aws_access = config_data["AWSPARMS"]
+s3resource = boto3.client("s3", aws_access_key_id=aws_access["accesskey"], aws_secret_access_key=aws_access["secretkey"])
+
+#Get customer details and saved as a json file
+#then upload to S3 buclet
+@app.route("/json_resp", methods=["GET"])
+def get_cust_details_and_upload_to_s3():
+    try:
+        all_cust_details = Custdata.query.all()
+        result = bank_schema.dump(all_cust_details)
+
+        resp_json_file = 'D:\\Flask_POC\\response\\resp.json'
+        for item in result:
+            output = json.dumps(item)
+
+            with open(resp_json_file, "a+") as outfile:
+                outfile.write(str(output))
+                outfile.close()
+
+        s3path = aws_access["s3path"] + "resp.json"
+        bucket = aws_access["s3bucketname"]
+        uploaded = upload_to_aws(resp_json_file, bucket, s3path)
+
+        if uploaded == "True":
+            os.remove(resp_json_file)
+        else:
+            logging.info("Could not unlink the json file")
+        return "Json file created and uploaded to s3 bucket successfully"
+    except Exception as e:
+        return(f"Unable to fetch the Customer Details {e}")
+
+#main function
 if __name__ == "__main__":
     app.run(debug=True)
